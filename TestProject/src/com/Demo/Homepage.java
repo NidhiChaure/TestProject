a5109bfb37366d1c0c560bbf419985d8a6c30818
NICKY <NICKY@DESKTOP-ITKI5LK>
2026-06-18 13:33:39 +0530

    PalindromeString commit

--- TestProject/src/com/Demo/Homepage.java
+++ TestProject/src/com/Demo/Homepage.java
@@ -6,14 +6,23 @@ public class Homepage {
 		
 		String st="Nidhi";
 		String rev="";
+		String temp=st;
 		for(int i=st.length()-1;i>=0;i--) {
 		    rev=rev+st.charAt(i);	
 			
 		}
 			System.out.println(rev);
+			if(temp.equals(rev))
+			{
+				System.out.println("palindrome String");
+			}
+			else
+			{
+				System.out.println("not palindrome string");
+			}
 		
-			int a=10, b=20,c=0;
-			c=a+b;
+			int a=10, b=2,c=0;
+			c=a/b;
 			System.out.println("the value of c is:"+c);
 
 }
