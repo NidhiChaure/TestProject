dbdf828bf522c46460ce552edc306a1060055eb3
NICKY <NICKY@DESKTOP-ITKI5LK>
2026-06-18 12:20:48 +0530

    String Program added

--- TestProject/src/com/Demo/Homepage.java
+++ TestProject/src/com/Demo/Homepage.java
@@ -4,6 +4,14 @@ public class Homepage {
 
 	public static void main(String[] args) {
 		
-	}
+		String st="Nidhi";
+		String rev="";
+		for(int i=st.length()-1;i>=0;i--) {
+		    rev=rev+st.charAt(i);	
+			
+		}
+			System.out.println(rev);
+		
 
+}
 }
